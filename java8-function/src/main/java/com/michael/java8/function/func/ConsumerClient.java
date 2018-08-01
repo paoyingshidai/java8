07991ddbf2e47c4e91557d7f855043a65946be03
paoyingshidai <13432849094@163.com>
2018-08-01 13:08:26 +0800

    添加 consumer 接口

---
+++ java8-function/src/main/java/com/michael/java8/function/func/ConsumerClient.java
@@ -0,0 +1,49 @@
+package com.michael.java8.function.func;
+
+import java.util.function.Consumer;
+
+public class ConsumerClient {
+
+	public static void main(String[] args) {
+
+		ConsumerTask task = new ConsumerTask(name -> {System.out.println("hello : " + name);});
+		task.sayHello();
+
+		MyConsumer1 c1 = new MyConsumer1();
+		MyConsumer2 c2 = new MyConsumer2();
+		task.andThen(c1, c2);
+
+	}
+}
+
+class ConsumerTask {
+
+	Consumer<String> consumer;
+
+	public ConsumerTask(Consumer<String> consumer) {
+		this.consumer = consumer;
+	}
+
+	public void sayHello() {
+		consumer.accept("Michael");
+	}
+
+	public void andThen(Consumer<String> c1, Consumer<String> c2) {
+		c1.andThen(c2).accept("Michael");
+	}
+}
+
+
+class MyConsumer1 implements Consumer<String> {
+	@Override
+	public void accept(String t) {
+		System.out.println("MyConsumer1 " + t);
+	}
+}
+
+class MyConsumer2 implements Consumer<String> {
+	@Override
+	public void accept(String t) {
+		System.out.println("MyConsumer2 " + t);
+	}
+}
